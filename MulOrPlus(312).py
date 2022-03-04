ae37136f5a6be580058613755c55bd3ad4c9486b
LaynardMerden <87759289+LaynardMerden@users.noreply.github.com>
2022-03-04 22:42:35 +0900

    Update MulOrPlus(312).py

--- MulOrPlus(312).py
+++ MulOrPlus(312).py
@@ -2,9 +2,7 @@ s = input()
 result = 0
 
 for i in s:
-    if result <= 1:
-        result += int(i)
-    elif int(i) <= 1:
+    if result <= 1 or int(i) <= 1:
         result += int(i)
     else:
         result *= int(i)
