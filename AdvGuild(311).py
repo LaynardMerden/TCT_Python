75b00bee09a27c3ceda69037669fe06a1dacda13
LaynardMerden <87759289+LaynardMerden@users.noreply.github.com>
2022-03-04 23:09:13 +0900

    Update AdvGuild(311).py

--- AdvGuild(311).py
+++ AdvGuild(311).py
@@ -1,19 +1,14 @@
-# n 입력 받음
 n = int(input()) 
-# n명의 모험가의 공포도 입력 받아서 arr에 list로 저장
 arr = list(map(int, input().split())) 
+arr.sort()
 
-cnt = 0 # idx 역할 (현재 그룹에 포함된 모험가의 수 저장)
-res = 0 # 현재 만들어진 그룹의 수
-arr.sort() # list를 오름차순으로 정렬. (공포도 낮은 모험가부터 하나씩 grouping)
+cnt = 0 # 현재 그룹에 포함된 모험가의 수
+res = 0 # 총 그룹의 수
 
-# 오름차순으로 정렬된 arr에 대해 공포도가 i인 경우 i 명씩 grouping 해 나가면서
-# cnt 와 res update (cnt < n)
-while cnt < n:
-    if arr[cnt] + cnt <= n:
-        cnt += arr[cnt]
-        res += 1
-    else:
-        break
+for i in data:    # 공포도 낮은 순으로 하나씩
+    cnt += 1      # i 공포도를 현재의 그룹에 넣어줌
+    if cnt >= i:  # 현재 그룹에 포함된 모험가의 수가 공포도 i 보다 크거나 같으면 
+        res += 1  # 그룹의 수를 늘려주고
+        cnt = 0   # 현재 그룹에 포함된 모험가의 수를 0으로 초기화
 
 print(res)
